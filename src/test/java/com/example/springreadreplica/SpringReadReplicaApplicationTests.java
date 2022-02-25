cb9eec57dc7cbab4a3956510b63620141a4c7f20
vikas_ver <vikas.ver@ascendcorp.com>
2022-02-25 23:28:13 +0700

    cleanup

--- src/test/java/com/example/springreadreplica/SpringReadReplicaApplicationTests.java
+++ src/test/java/com/example/springreadreplica/SpringReadReplicaApplicationTests.java
@@ -1,9 +1,7 @@
 package com.example.springreadreplica;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
 class SpringReadReplicaApplicationTests {
 
 	@Test
