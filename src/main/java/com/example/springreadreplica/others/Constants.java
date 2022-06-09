5e4d8e0c4c78b3c5113f48c502559ebeadb5a504
vikas_ver <vikas.ver@ascendcorp.com>
2022-06-09 21:13:34 +0700

    base

--- src/main/java/com/example/springreadreplica/others/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.example.springreadreplica.others;
-
-public interface Constants {
-  String url = "jdbc:mysql://alpha-merchant-profile-db.tmn-dev.com/mapi";
-  String readOnlyDbUrl = "jdbc:mysql://localhost:3306/mapi";
-
-}
--- src/test/java/com/example/springreadreplica/SpringReadReplicaApplicationTests.java
+++ src/test/java/com/example/springreadreplica/SpringReadReplicaApplicationTests.java
@@ -2,14 +2,26 @@ package com.example.springreadreplica;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.function.*;
+
 class SpringReadReplicaApplicationTests {
 
-	@Test
-	void contextLoads() {
+  @Test
+  void contextLoads() {
+  }
+
+	static int invokeAGod(int a, int b, int c) {
+		throw new StackOverflowError("XXXXXX");
 	}
+	static int a, b, c;
 
 	@Test
-	public void hello() {
-		System.out.println("hello");
+  public void hello() {
+		try {
+			invokeAGod(a = 1, b = 2, c = 3);
+		}catch(Exception e){
+			System.out.println("Handled error");
+		}
 	}
+
 }
