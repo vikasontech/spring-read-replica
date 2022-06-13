0d6f36bea9e3867571970c9cba52134b1eb30450
vikas_ver <vikas.ver@ascendcorp.com>
2022-06-13 23:05:58 +0700

    cleaup the code

---
+++ src/main/java/com/example/springreadreplica/config/AppConstants.java
@@ -0,0 +1,13 @@
+package com.example.springreadreplica.config;
+
+
+public abstract class AppConstants {
+    private AppConstants() {
+        //do nothing
+    }
+
+    public static String MASTER = "master";
+    //    public static String MASTER_DATA_SOURCE = "masterDataSource";
+    public static String REPLICA = "replica";
+
+}
